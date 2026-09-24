43c0df306202ce4f8ed7d6145ef2c65f7b1359f8
Tharlesson <tharlesson.souza@tqi.com.br>
2026-09-24 09:43:49 -0300

    fix(auth): remover groups dos scopes OIDC

--- apps/web/public/config.js
+++ apps/web/public/config.js
@@ -2,5 +2,5 @@ window.__SENTINEL_CONFIG__ = {
   authMode: 'oidc',
   oidcAuthority: 'https://login.microsoftonline.com/f165beb9-b183-4037-9abe-dca616ef2645/v2.0',
   oidcClientId: '7cadec8a-4297-4822-ba03-fdb7717094fb',
-  oidcScope: 'openid profile groups api://7cadec8a-4297-4822-ba03-fdb7717094fb/sentinelops.api'
+  oidcScope: 'openid profile api://7cadec8a-4297-4822-ba03-fdb7717094fb/sentinelops.api'
 };
